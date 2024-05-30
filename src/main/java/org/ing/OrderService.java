2b34c28c4f1bc8e7a84e0446555c609554bd748b
Ingmar Fjolla <ingmar.fjolla09@myhunter.cuny.edu>
2024-05-30 10:29:02 -0400

    added back debezium

--- src/main/java/org/ing/OrderService.java
+++ src/main/java/org/ing/OrderService.java
@@ -1,25 +1,21 @@
 package org.ing;
-//import io.debezium.outbox.reactive.quarkus.internal.DebeziumOutboxHandler;
-
+import io.debezium.outbox.reactive.quarkus.internal.DebeziumOutboxHandler;
 import io.quarkus.hibernate.reactive.panache.common.WithTransaction;
 import io.quarkus.logging.Log;
 import io.smallrye.mutiny.Uni;
-
 import jakarta.enterprise.context.ApplicationScoped;
 import jakarta.inject.Inject;
-        import org.ing.repository.OrderRepository;
+import org.ing.cdc.OrderCreatedEvent;
+import org.ing.repository.OrderRepository;
 import org.ing.repository.models.Order;
-//import org.ingi.cdc.OrderCreatedEvent;
+import java.util.UUID;
 
-
-        import java.util.UUID;
-//import io.debezium.outbox.quarkus.ExportedEvent;
 
 
 @ApplicationScoped
 public class OrderService {
-//    @Inject
-//    DebeziumOutboxHandler handler;
+    @Inject
+    DebeziumOutboxHandler handler;
 
     OrderRepository orderRepository;
 
@@ -30,8 +26,8 @@ public class OrderService {
     @WithTransaction
     public Uni<Order> create(Order order){
         Log.info(Thread.currentThread().getName());
-        return this.orderRepository.persistAndFlush(order);
-//                .call(() -> handler.persistToOutbox(OrderCreatedEvent.of(order)));
+        return this.orderRepository.persistAndFlush(order)
+                .call(() -> handler.persistToOutbox(OrderCreatedEvent.of(order)));
     }
 
 
